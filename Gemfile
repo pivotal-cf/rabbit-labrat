be5892bacbe3ba87bebd137bd31b48efa907edfb
Michael Klishin <mklishin@gopivotal.com>
2013-09-11 11:26:22 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "puma"
+gem "sinatra-contrib"
+gem "erubis"
---
+++ main.rb
@@ -0,0 +1,16 @@
+require "sinatra/base"
+
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+class LabRat < Sinatra::Base
+  require 'sinatra/reloader' if development?
+
+  configure do
+    set :server, :puma
+    set :views,  File.join(settings.root, 'views')
+  end
+
+  get "/" do
+    erb :index
+  end
+end
