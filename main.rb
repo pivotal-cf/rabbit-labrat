dab883719bfc4998bdd719bc5395ccbd6598b354
Michael Klishin <mklishin@gopivotal.com>
2013-10-18 16:04:35 +0400

    Rework RabbitMQ service health page, provide JSON endpoints
    Fixes #4.

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require "sinatra/base"
 require "multi_json"
 require "bunny"
 
+$LOAD_PATH << "./lib"
+
 # For development
 if File.exists?("vcap_services.json") && File.readable?("vcap_services.json")
   ENV["VCAP_SERVICES"] = MultiJson.dump(MultiJson.load(File.read("vcap_services.json")))
@@ -10,6 +12,7 @@ end
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 class LabRat < Sinatra::Base
+  require "lab_rat/health_checker"
   require 'sinatra/reloader' if development?
 
   configure do
@@ -17,37 +20,51 @@ class LabRat < Sinatra::Base
     set :views,  File.join(settings.root, 'views')
   end
 
+  helpers do
+    def svs
+      MultiJson.load(ENV["VCAP_SERVICES"])
+    end
+
+    def creds
+      creds = svs["rabbitmq-1.0"].
+        map { |h| h["credentials"] }
+    end
+  end
+
   get "/" do
     erb :index
   end
 
+  get "/vcap/services.json" do
+    MultiJson.dump(ENV["VCAP_SERVICES"])
+  end
+
   get "/services/rabbitmq" do
     if ENV["VCAP_SERVICES"]
-      svs  = MultiJson.load(ENV["VCAP_SERVICES"])
-      uri  = svs["rabbitmq-1.0"].first["credentials"]["uri"]
-      conn = Bunny.new(uri, :verify_peer => false)
-      begin
-        conn.start
+      hc      = HealthChecker.new
+      results = creds.map { |c| hc.check(c) }
 
-        ch   = conn.create_channel
-        q    = ch.queue("", :exclusive => true)
-
-        erb :rabbitmq_service, :locals => {
-          :healthy    => true,
-          :connection => conn,
-          :channel    => ch,
-          :queue      => q
-        }
-      rescue Bunny::PossibleAuthenticationFailureError => e
-        erb :rabbitmq_service, :locals => {
-          :healthy    => false,
-          :connection => nil
-        }
-      end
-    else
       erb :rabbitmq_service, :locals => {
-        :healthy => false
+        :results => results
       }
+    end
+  end
+
+  get "/services/rabbitmq.json" do
+    if ENV["VCAP_SERVICES"]
+      hc      = HealthChecker.new
+      results = creds.
+        map { |c| hc.check(c) }.
+        map do |h|
+        # modify objects that cannot be serialized to JSON
+        h[:connection] = nil
+        h[:queue] = h[:queue].name
+
+        h
+        end
+
+
+      MultiJson.dump(results.to_json)
     end
   end
 end
