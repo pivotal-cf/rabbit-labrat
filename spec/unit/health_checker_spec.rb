624558d6ae402ce980d322eda988855cf7d747e2
Michael Klishin <mklishin@gopivotal.com>
2013-11-13 15:15:17 +0400

    Update health checker tests

--- spec/unit/health_checker_spec.rb
+++ spec/unit/health_checker_spec.rb
@@ -1,21 +1,25 @@
 require "spec_helper"
 
-require "lab_rat/health_checker"
+require "lab_rat/aggregate_health_checker"
 
 
-describe LabRat::HealthChecker do
+describe LabRat::AggregateHealthChecker do
   context "with valid credentials" do
     let(:credentials) do
-      {
-        "uri"          => "amqp://guest:guest@127.0.0.1",
-        "http_api_uri" => "http://guest:guest@127.0.0.1:15672/api"
-      }
+      [{
+          "uri"   => "amqp://guest:guest@127.0.0.1",
+          :proto  => "amqp"
+        },
+        {
+          :proto  => "management",
+          "uri"   => "http://guest:guest@127.0.0.1:15672/api"
+        }]
     end
 
     it "returns check results as a map" do
-      m = subject.check(credentials)
+      m = subject.check(credentials).first
 
-      m[:uri].should == credentials["uri"]
+      m[:uri].should == credentials.first["uri"]
       m[:connection].should be_connected
 
       m[:queue].should_not be_nil
@@ -28,14 +32,18 @@ describe LabRat::HealthChecker do
 
   context "with invalid credentials" do
     let(:credentials) do
-      {
-        "uri"          => "amqp://guest:guest@128.99.0.1",
-        "http_api_uri" => "http://guest:guest@128.76.0.1:15678/api"
-      }
+      [{
+          "uri"   => "amqp://guest:guest@127.0.87.9",
+          :proto  => "amqp"
+        },
+        {
+          :proto  => "management",
+          "uri"   => "http://guest:guest@192.168.80.91:15678/api"
+        }]
     end
 
     it "returns check results with :exception" do
-      m = subject.check(credentials)
+      m = subject.check(credentials).first
 
       m[:exception].should_not be_nil
     end
